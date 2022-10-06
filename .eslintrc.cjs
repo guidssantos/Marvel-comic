5e9e30e322ec54cd4eac91b7555eb096660d2069
guidssantos <guilhermedssantos1@gmail.com>
2022-10-06 08:31:20 -0300

    Implementação do prettier e eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  overrides: [],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'jsx-quotes': ['error', 'prefer-single'],
+  },
+};
