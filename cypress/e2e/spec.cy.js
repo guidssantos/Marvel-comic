9cb7ce3beadbb1776a93ac7eddb502e6e95e7525
guidssantos <guilhermedssantos1@gmail.com>
2022-10-10 22:20:26 -0300

    testes iniciais com cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.visit('/');
+    cy.contains('MARVEL COMICS').should('to.have.length', 1);
+    cy.get(
+      ':nth-child(4) > .sc-bjfHbI > :nth-child(2) > a > .sc-dIfARi'
+    ).click();
+    cy.get('.sc-bYMpWt').click();
+    cy.get('.sc-jSUZER > .sc-gKPRtg > a > .sc-iBYQkv > path').click();
+    cy.get(':nth-child(4) > .sc-cabOPr').contains('$ 17.99');
+    cy.get('.sc-cabOPr > :nth-child(1)').click();
+    cy.get(':nth-child(3) > .sc-cabOPr').contains(2);
+  });
+});
