68d0c446cc662196f7d2a44799e2f4951f01dc39
guidssantos <guilhermedssantos1@gmail.com>
2022-10-12 18:22:28 -0300

    Novos testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -21,17 +21,31 @@ describe('Navigation UI', () => {
     cy.get('.sc-dIfARi').should('to.have.length', 1);
     cy.get('.sc-bcXHqe').should('to.have.length', 1);
     cy.get('.sc-ezOQGI').should('to.have.length', 1);
-    cy.get('.sc-bYMpWt').should('to.have.length', 1).click();
+    cy.get('.sc-jIRcFI').should('to.have.length', 1).click();
   });
   it('Should return true if it contains components and is correctly calculating the price', () => {
     cy.get('.sc-jSUZER > .sc-gKPRtg > a > strong').should('to.have.text', 1);
     cy.get('.sc-jSUZER > .sc-gKPRtg > a > .sc-iBYQkv').click();
-    cy.get('.sc-jIRcFI').should('to.have.length', 1);
-    cy.get('.sc-AHaJN')
+    cy.get('.sc-cCjUiG').should('to.have.length', 1);
+    cy.get('.sc-gScZFl')
       .should('to.have.length', 1)
-      .should('to.have.text', 'SUBTOTAL: $ 3.99');
-    cy.get('.sc-iTFTee').click();
-    cy.get(':nth-child(3) > .sc-cabOPr').should('to.have.text', 2);
-    cy.get('.sc-AHaJN').should('to.have.text', 'SUBTOTAL: $ 7.98');
+      .should('to.have.text', 'SUBTOTAL:  $ 3.99');
+    cy.get(':nth-child(3) > .sc-eeMvmM > :nth-child(1)').click();
+    cy.get(':nth-child(3) > .sc-eeMvmM').should('to.have.text', 2);
+    cy.get('.sc-gScZFl').should('to.have.text', 'SUBTOTAL:  $ 7.98');
+  });
+  it('Should return true when clicking the remove button and the component is removed', () => {
+    cy.get('.sc-dPWrhe').click();
+    cy.get('.sc-cCjUiG').should('to.have.length', 0);
+  });
+  it('Should return true if clicking on checkout goes to the payment done screen', () => {
+    cy.visit('/');
+    cy.get(
+      ':nth-child(4) > .sc-bjfHbI > :nth-child(2) > a > .sc-dIfARi'
+    ).click();
+    cy.get('.sc-jIRcFI').click();
+    cy.get('.sc-jSUZER > .sc-gKPRtg > a').click();
+    cy.get('.sc-lbVpMG').click();
+    cy.get('.sc-hiDMwi').should('to.have.length', 1);
   });
 });
